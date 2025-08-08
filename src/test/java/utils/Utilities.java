cde3feb8133f2aad35be0a9b5d10d006a0a09d25
Gabriel Carmona <gcarmona@MacBook-Pro-de-Gabriel.local>
2025-08-08 11:03:35 -0400

    Creación del primer Escenario en Gherkin - First Gherkin Scenario created

---
+++ src/test/java/utils/Utilities.java
@@ -0,0 +1,23 @@
+package utils;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class Utils extends driverFactory {
+
+    public void alertAccept(){
+        try{
+            WebDriverWait wait =new WebDriverWait(driver, Duration.ofSeconds(5));
+            wait.until(ExpectedConditions.alertIsPresent());
+            Alert alert = driver.switchTo().alert();
+            alert.accept();
+        } catch (NoAlertPresentException e) {
+            System.out.println("No se encontró el elemento Alert");
+        }
+    }
+
+}
